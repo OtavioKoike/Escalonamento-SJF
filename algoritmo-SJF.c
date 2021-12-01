88898ba6a23f04b6420e8bc79e9fb458715f6c66
OtavioKoike <otaviokoike07@gmail.com>
2021-12-01 16:56:43 -0300

    Struct Processos

--- algoritmo-SJF.c
+++ algoritmo-SJF.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct processos {
+	int numero;					// Numero do processo para melhor identificacao
+	int criacao;				// Tempo em que o processo entrou na fila
+	int execucao;				// Duracao do processo
+	int prioridade;				// Prioridade do processo
+	int espera;					// Tempo de espera para ser executado
+	int vida;					// Tempo de vida do processo
+	struct processos *prox;		// Proximo processo na fila
+};
+
 main(int argc, char *argv[]){
 	// criando a variável ponteiro para o arquivo
 	FILE *arq;
